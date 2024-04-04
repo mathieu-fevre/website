c4634b1f8b879015adaad658acad120dd7dd058b
Mathieu Fevre <m_fevre@internetacademy.co.jp>
2024-04-04 14:49:41 +0900

    allowed hosts

--- website/urls.py
+++ website/urls.py
@@ -26,5 +26,5 @@ urlpatterns = [
     path("home", TemplateView.as_view(template_name="home.html"), name="home"),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
