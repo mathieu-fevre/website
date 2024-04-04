43415c753698b10dad9e08fc04f57d958a16da26
Mathieu Fevre <m_fevre@internetacademy.co.jp>
2024-04-04 16:23:57 +0900

    changed home

--- website/urls.py
+++ website/urls.py
@@ -23,7 +23,7 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
-    path("home", TemplateView.as_view(template_name="home.html"), name="home"),
+    path("", TemplateView.as_view(template_name="home.html"), name="home"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
