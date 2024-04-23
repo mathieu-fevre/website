e742e06acb36f7c1c74078f8adb6a0a079478ad8
Mathieu Fevre <m_fevre@internetacademy.co.jp>
2024-04-23 14:43:02 +0900

    resolving other problems

--- website/apps/blackjack/urls.py
+++ website/apps/blackjack/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name='core'
+app_name='blackjack'
 urlpatterns = [
     path('display_basic_strategy', views.display_basic_strategy, name='display_basic_strategy'),
 ]
