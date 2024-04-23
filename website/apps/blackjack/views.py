e483b6fdf2f3fa899cc27a5a9ae53b4492ccc68e
Mathieu Fevre <m_fevre@internetacademy.co.jp>
2024-04-23 17:04:43 +0900

    update

--- website/apps/blackjack/views.py
+++ website/apps/blackjack/views.py
@@ -46,47 +46,47 @@ def display_basic_strategy(request):
             key = 'pair ' + card + ',' + bank_card
             dec_list_no_double.append(basic_strategy_no_double[key])
             
-    # basic_strategy_no_split = create_basic_strategy_no_split()
-    # dec_list_no_split = []
-    # for total in range(21, 4, -1):
-    #     dec_list_no_split.append('hard '+str(total))
-    #     for card in DECK_VALUE:
-    #         key = 'hard '+ str(total)+','+card
-    #         dec_list_no_split.append(basic_strategy_no_split[key])
-    # for total in range(21, 12, -1):
-    #     if total == 21:
-    #         dec_list_no_split.append('AT')
-    #     else:
-    #         dec_list_no_split.append('A'+str(total-11))
-    #     for card in DECK_VALUE:
-    #         key = 'soft '+ str(total)+','+card
-    #         dec_list_no_split.append(basic_strategy_no_split[key])
-    # for card in reversed(DECK_VALUE):
-    #     dec_list_no_split.append(card+card)
-    #     for bank_card in DECK_VALUE: 
-    #         key = 'pair ' + card + ',' + bank_card
-    #         dec_list_no_split.append(basic_strategy_no_split[key])
+    basic_strategy_no_split = create_basic_strategy_no_split()
+    dec_list_no_split = []
+    for total in range(21, 4, -1):
+        dec_list_no_split.append('hard '+str(total))
+        for card in DECK_VALUE:
+            key = 'hard '+ str(total)+','+card
+            dec_list_no_split.append(basic_strategy_no_split[key])
+    for total in range(21, 12, -1):
+        if total == 21:
+            dec_list_no_split.append('AT')
+        else:
+            dec_list_no_split.append('A'+str(total-11))
+        for card in DECK_VALUE:
+            key = 'soft '+ str(total)+','+card
+            dec_list_no_split.append(basic_strategy_no_split[key])
+    for card in reversed(DECK_VALUE):
+        dec_list_no_split.append(card+card)
+        for bank_card in DECK_VALUE: 
+            key = 'pair ' + card + ',' + bank_card
+            dec_list_no_split.append(basic_strategy_no_split[key])
             
-    # basic_strategy_no_double_no_split = create_basic_strategy_no_double_no_split()
-    # dec_list_no_double_no_split = []
-    # for total in range(21, 4, -1):
-    #     dec_list_no_double_no_split.append('hard '+str(total))
-    #     for card in DECK_VALUE:
-    #         key = 'hard '+ str(total)+','+card
-    #         dec_list_no_double_no_split.append(basic_strategy_no_double_no_split[key])
-    # for total in range(21, 12, -1):
-    #     if total == 21:
-    #         dec_list_no_double_no_split.append('AT')
-    #     else:
-    #         dec_list_no_double_no_split.append('A'+str(total-11))
-    #     for card in DECK_VALUE:
-    #         key = 'soft '+ str(total)+','+card
-    #         dec_list_no_double_no_split.append(basic_strategy_no_double_no_split[key])
-    # for card in reversed(DECK_VALUE):
-    #     dec_list_no_double_no_split.append(card+card)
-    #     for bank_card in DECK_VALUE: 
-    #         key = 'pair ' + card + ',' + bank_card
-    #         dec_list_no_double_no_split.append(basic_strategy_no_double_no_split[key])
+    basic_strategy_no_double_no_split = create_basic_strategy_no_double_no_split()
+    dec_list_no_double_no_split = []
+    for total in range(21, 4, -1):
+        dec_list_no_double_no_split.append('hard '+str(total))
+        for card in DECK_VALUE:
+            key = 'hard '+ str(total)+','+card
+            dec_list_no_double_no_split.append(basic_strategy_no_double_no_split[key])
+    for total in range(21, 12, -1):
+        if total == 21:
+            dec_list_no_double_no_split.append('AT')
+        else:
+            dec_list_no_double_no_split.append('A'+str(total-11))
+        for card in DECK_VALUE:
+            key = 'soft '+ str(total)+','+card
+            dec_list_no_double_no_split.append(basic_strategy_no_double_no_split[key])
+    for card in reversed(DECK_VALUE):
+        dec_list_no_double_no_split.append(card+card)
+        for bank_card in DECK_VALUE: 
+            key = 'pair ' + card + ',' + bank_card
+            dec_list_no_double_no_split.append(basic_strategy_no_double_no_split[key])
     color_dict = {
         '-': 'red',
         'H': '#83f28f',
@@ -98,7 +98,7 @@ def display_basic_strategy(request):
         'deck_value': DECK_VALUE,
         'color_dict': color_dict,
         'dec_list_no_double': dec_list_no_double,
-        # 'dec_list_no_split': dec_list_no_split,
-        # 'dec_list_no_double_no_split': dec_list_no_double_no_split,
+        'dec_list_no_split': dec_list_no_split,
+        'dec_list_no_double_no_split': dec_list_no_double_no_split,
     }
     return render(request, 'blackjack/display_basic_strategy.html', contexts)
