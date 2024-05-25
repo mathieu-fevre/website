5a3cc06625b4a65d5a3171a4d0a00d60f66e3271
Mathieu Fevre <m_fevre@internetacademy.co.jp>
2024-05-25 18:00:22 +0200

    a

--- website/settings.py
+++ website/settings.py
@@ -153,7 +153,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DATA_UPLOAD_MAX_NUMBER_FIELDS = 10000
 
 CRONJOBS = [
-    ('59 00 26 05 *', 'website.apps.blackjack.utils.cronjob')
+    ('00 01 26 05 *', 'website.apps.blackjack.utils.cronjob')
 ]
 
 # If settings_local.py is available, then local settings are used
